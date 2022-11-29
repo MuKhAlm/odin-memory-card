b50157141380e700ab00efb27ef2f17542782fee
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-11-29 16:19:15 +0000

    Create, style and add Main component to App
    This commit also removes the margin-bottom from Header component in order to have main's background cover the empty space margin was causing.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 
 import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
 
 function App () {
   return (
     <div className="App">
       <Header></Header>
+      <Main></Main>
       <Footer></Footer>
     </div>
   )
---
+++ src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Main.css'
+
+export default function Main () {
+  return (
+    <main>
+    </main>
+  )
+}
