6c7523cb9fee2da8da52c6a242b06ced1eec8631
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-11-29 15:57:00 +0000

    Create, style and add Header component to App
    This commit also adds basic styling for App

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header () {
+  return (
+    <header>
+      <h1>
+        Memory Card
+      </h1>
+    </header>
+  )
+}
