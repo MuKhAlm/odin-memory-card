67de382a9edc4732310a01e8bf67d8bf0cd0b82e
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-11-29 16:15:02 +0000

    Create, style and add Footer component to App
    This commit also changes the app's title in Index.html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
+
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App () {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer () {
+  return (
+    <footer>
+      <a href='https://github.com/MuKhAlt/odin-memory-card.git' target={'_blank'} rel="noreferrer" >
+        <i className='fa-github fab'></i>
+      </a>
+    </footer>
+  )
+}
