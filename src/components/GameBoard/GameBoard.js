8a23ddf7b3546f1f97c87679d0c582e90e3e4188
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-11-30 18:46:24 +0000

    Implement shape click event handler for GB
    The handler is now responsible for the game logic (except updating the score which will be implemented later) when a shape is clicked.

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -5,12 +5,38 @@ import Card from '../Card/Card'
 import generateRandomIntegers from '../../utils/utils'
 
 export default function GameBoard () {
-  const handleShapeClick = (shapeID) => {
-    console.log(shapeID)
-  }
-
   // eslint-disable-next-line no-unused-vars
   const [shapes, setShapes] = useState(generateRandomIntegers(8))
+  // eslint-disable-next-line no-unused-vars
+  const [clickedShapes, setClickedShapes] = useState([])
+
+  /**
+   * Handles shape click events,
+   *
+   * This function is responsible for trigerring state and score updates based on clicked shape
+   * @param {Number} shapeID the ID of the clicked shape
+   * @modifies shapes (state), clickedShapes (state)
+   */
+  const handleShapeClick = (shapeID) => {
+    if (!clickedShapes.includes(shapeID)) {
+      // Add shape to clicked shapes
+      setClickedShapes([...clickedShapes, shapeID])
+
+      // Rearrange shapes
+      setShapes(generateRandomIntegers(8))
+
+      // TODO: Update score (and best score)
+    } else {
+      // Reset clicked shapes
+      setClickedShapes([])
+
+      // Rearrange shapes
+      setShapes(generateRandomIntegers(8))
+
+      // TODO: Reset score
+    }
+  }
+
   return (
     <div id='gameBoard' >
       <Card shapeID={shapes[0]} onClick={ handleShapeClick }></Card>
