e686922b2c4608ea347a3f9cd2854dd514d090e9
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-11-30 17:16:25 +0000

    Create Card component and add it to GameBoard
    The Card component changes the shape displayed based on given props (by GameBoard).
    Style was added to GameBoard and Card in order to make the display work.
    
    Things to consider: With the way style was implemented, the ratios of the shape can change depending on the ratio of GameBoard, this can be problematic for shapes such as square and circle, hence, circle was changed to ellipse (no issues seen with square yet).

---
+++ src/components/Card/Card.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+import './Card.css'
+import ellipse from '../../assets/ellipse.svg'
+import square from '../../assets/square.svg'
+import triangle from '../../assets/triangle.svg'
+import rectangle from '../../assets/rectangle.svg'
+import star from '../../assets/star.svg'
+import rhombus from '../../assets/rhombus.svg'
+import octagon from '../../assets/octagon.svg'
+import heptagon from '../../assets/heptagon.svg'
+
+export default function Card (props) {
+  const shapes = [ellipse, square, triangle, rectangle, star, rhombus, octagon, heptagon]
+  return (
+    <div id='card' >
+      <img src={shapes[props.shapeID]} alt='shape' />
+    </div>
+  )
+}
--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 import './GameBoard.css'
+import Card from '../Card/Card'
 
 export default function GameBoard () {
   return (
-    <div id='gameBoard' ></div>
+    <div id='gameBoard' >
+      <Card shapeID={0}></Card>
+      <Card shapeID={1}></Card>
+      <Card shapeID={2}></Card>
+      <Card shapeID={3}></Card>
+      <Card shapeID={4}></Card>
+      <Card shapeID={5}></Card>
+      <Card shapeID={6}></Card>
+      <Card shapeID={7}></Card>
+    </div>
   )
 }
